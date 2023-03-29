bbfe7e1d21b1b772144da4035ac2eb4b94955658
abdulbari149 <abdulbari131103@gmail.com>
2023-03-29 13:04:02 +0500

    logo added

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,15 +1,9 @@
-import React from "react";
-import {
-	Navbar,
-	Nav,
-	NavItem,
-	Form,
-} from "react-bootstrap";
-import { sizes } from "../../constant/sizes";
-import { useTheme } from "../../hook/useTheme";
-import styled from "styled-components";
-import useMediaQuery from "../../hook/useMediaQuery";
-
+import React from 'react';
+import { Navbar, Nav, NavItem, Form } from 'react-bootstrap';
+import { sizes } from '../../constant/sizes';
+import { useTheme } from '../../hook/useTheme';
+import styled from 'styled-components';
+import useMediaQuery from '../../hook/useMediaQuery';
 
 interface Props {
 	handleSidebar: () => void;
@@ -27,10 +21,20 @@ const Header: React.FunctionComponent<Props> = ({ handleSidebar }) => {
 		<AppHeader backgroundColor={`${colors.bg.primary}`}>
 			<AppNavbar expand="xl">
 				<Navbar.Toggle onClick={handleToggleClick} />
-				<Navbar.Brand style={{ color: "white" }}>WSDA SQL Editor</Navbar.Brand>
+				<Navbar.Brand style={{ color: 'white', margin: '0 15px' }}>
+					<a href="https://wsdalearning.ai">
+						<img
+							src={`${process.env.REACT_APP_PUBLIC_URL}/logo-light.png`}
+							style={{ marginRight: '25px' }}
+							alt="wsda logo"
+							width={180}
+						/>
+					</a>
+					WSDA SQL Editor
+				</Navbar.Brand>
 				<Nav>
 					<NavItem className="mode m-auto ms-sm-auto me-sm-0 d-flex">
-						{media ? "Mode" : ""}
+						{media ? 'Mode' : ''}
 						<CustomSwitch onChange={toggleMode} />
 					</NavItem>
 				</Nav>
@@ -40,13 +44,16 @@ const Header: React.FunctionComponent<Props> = ({ handleSidebar }) => {
 };
 
 const CustomSwitch = styled(Form.Switch)`
-	margin-left: 10px;
-	& .form-check-input:checked {
-		background-color: #0d0d0d;
-	}
+     margin-left: 10px;
+     & .form-check-input:checked {
+          background-color: #0d0d0d;
+     }
 `;
 
-const AppHeader = styled.div<{ backgroundColor: string }>`
+const AppHeader =
+	styled.div <
+		{ backgroundColor: string } >
+		`
 	width: calc(100% - 20px);
 	height: calc(100% - 16px);
 	grid-column: 2 / -1;
@@ -59,57 +66,56 @@ const AppHeader = styled.div<{ backgroundColor: string }>`
 `;
 
 const AppNavbar = styled(Navbar)`
-	--scale: 62.5%;
-	--offset: 2em;
-	padding-left: var(--offset);
-	padding-right: var(--offset);
-	color: white;
-	height: 100%;
-	width: 100%;
-	display: grid !important;
-	grid-auto-flow: column;
-	grid-template-columns: repeat(2, 1fr);
-	font-size: var(--scale);
-	place-content: center;
-	grid-gap: 2em;
+     --scale: 62.5%;
+     --offset: 2em;
+     padding-left: var(--offset);
+     padding-right: var(--offset);
+     color: white;
+     height: 100%;
+     width: 100%;
+     display: grid !important;
+     grid-auto-flow: column;
+     grid-template-columns: repeat(2, 1fr);
+     font-size: var(--scale);
+     place-content: center;
+     grid-gap: 2em;
 
-	& .navbar-nav {
-		margin-left: auto;
-	}
+     & .navbar-nav {
+          margin-left: auto;
+     }
 
-	& span.navbar-brand {
-		font-size: 2.4em;
-	}
+     & span.navbar-brand {
+          font-size: 2.4em;
+     }
 
-	& .mode {
-		font-size: 2.2em;
-	}
+     & .mode {
+          font-size: 2.2em;
+     }
 
-	@media screen and (max-width: ${sizes.xl}) {
-		grid-template-columns: 50px repeat(2, 1fr);
-	}
+     @media screen and (max-width: ${sizes.xl}) {
+          grid-template-columns: 50px repeat(2, 1fr);
+     }
 
-	@media screen and (min-width: ${sizes.xs}) and (max-width: ${sizes.sm}) {
-		--scale: 52%;
-		grid-gap: 1em;
-		& *:first-child {
-			grid-row: 1 / -1;
-		}
+     @media screen and (min-width: ${sizes.xs}) and (max-width: ${sizes.sm}) {
+          --scale: 52%;
+          grid-gap: 1em;
+          & *:first-child {
+               grid-row: 1 / -1;
+          }
 
-		& *:not(:first-child) {
-			place-self: end;
-		}
-	}
+          & *:not(:first-child) {
+               place-self: end;
+          }
+     }
 
-	@media screen and (max-width: ${sizes.xs}) {
-		--scale: 45%;
-		
-		grid-template-columns: 40px max-content 1fr;
-		& .navbar-toggler {
-			padding: 2px !important;
-		}
-	}
+     @media screen and (max-width: ${sizes.xs}) {
+          --scale: 45%;
+
+          grid-template-columns: 40px max-content 1fr;
+          & .navbar-toggler {
+               padding: 2px !important;
+          }
+     }
 `;
-
 
 export default Header;
